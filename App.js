dca1490cffe91799f70fe5e67864d8e9bc2766e0
rydfdli <84863510+rydfdli@users.noreply.github.com>
2024-01-02 22:14:41 +0800

    Update App.js

--- App.js
+++ App.js
@@ -2,9 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 const axios = require('axios')
-const cors = require('cors');
-
-app.use(cors());
 
 
 // WELLCOME TO MLBB API
